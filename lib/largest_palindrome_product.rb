e55b53c6169c62ccb348799e77c7f19593130b78
github username <github email address>
2019-12-16 10:03:19 -0500

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,22 @@
 # Implement your procedural solution here!
+# Your objective is to find the largest palindrome made from the product of two three-digit numbers.
+
+def largest_palindrome_product
+  a = (100..999)
+  b = (100..999)
+  palindrome = []
+
+  a.map do |anum|
+    b.map do |bnum|
+      c = bnum * anum
+      if c.to_s == c.to_s.reverse
+        palindrome = palindrome.push(c)
+      end
+      palindrome
+    end
+  end
+  palindrome.sort.last
+  
+end
+
+largest_palindrome_product()
